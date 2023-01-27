8b430460b2f09255a128410eca299df188633383
Berry Phillips <berry@berryp.xyz>
2023-01-27 18:54:51 +0900

    Fix rancher-desktop

--- pkgs/rancher-desktop/default.nix
+++ pkgs/rancher-desktop/default.nix
@@ -1,35 +1,32 @@
-{
-  lib,
-  stdenv,
-  fetchurl,
-  undmg,
-}: let
+{ lib, stdenv, fetchurl, undmg, unzip }:
+
+let
   arch = lib.strings.removeSuffix "-darwin" stdenv.system;
 in
-  stdenv.mkDerivation rec {
-    pname = "rancher-desktop";
-    version = "1.7.0";
+stdenv.mkDerivation rec {
+  pname = "rancher-desktop";
+  version = "1.7.0";
 
-    src = fetchurl {
-      url = "https://github.com/rancher-sandbox/rancher-desktop/releases/download/v${version}/Rancher.Desktop-${version}.${arch}.dmg";
-      sha256 = "sha256-go3eRIaMPDP+cJ4Jn5rwgBQ6N5+fui47rNhkH1rY5ys=";
-    };
+  src = fetchurl {
+    url = "https://github.com/rancher-sandbox/rancher-desktop/releases/download/v${version}/Rancher.Desktop-${version}.${arch}.dmg";
+    sha256 = "sha256-go3eRIaMPDP+cJ4Jn5rwgBQ6N5+fui47rNhkH1rY5ys=";
+  };
 
-    nativeBuildInputs = [undmg];
+  nativeBuildInputs = [ undmg unzip ];
+  sourceRoot = ".";
+  phases = [ "unpackPhase" "installPhase" ];
 
-    sourceRoot = ".";
+  installPhase = ''
+    mkdir -p "$out/Applications/"
+    cp -pR *.app "$out/Applications/"
+  '';
 
-    installPhase = ''
-      mkdir -p $out/Applications
-      cp -R Rancher\ Desktop.app $out/Applications
-    '';
-
-    meta = with lib; {
-      description = "Rancher Desktop";
-      homepage = "https://www.resilio.com/";
-      sourceProvenance = with sourceTypes; [binaryNativeCode];
-      license = licenses.unfreeRedistributable;
-      platforms = ["aarch64-darwin" "x86_64-darwin"];
-      maintainers = with maintainers; [berryp];
-    };
-  }
+  meta = with lib; {
+    description = "Rancher Desktop";
+    homepage = "https://www.resilio.com/";
+    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
+    license = licenses.unfreeRedistributable;
+    platforms = [ "aarch64-darwin" "x86_64-darwin" ];
+    maintainers = with maintainers; [ berryp ];
+  };
+}
